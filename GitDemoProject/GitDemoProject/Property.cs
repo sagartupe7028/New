bd5b4ee76900b45b7e36434983cccfe1d4d3faf4
sagartupe7028 <79117390+sagartupe7028@users.noreply.github.com>
2024-06-03 14:20:23 +0530

    Add files via upload
    project added

---
+++ GitDemoProject/GitDemoProject/Property.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemoProject
+{
+   public class Property
+    {
+        public string name { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
